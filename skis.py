3807dd3ef196fbfce452597670a150974c64e99a
Matthieu Fraiz <matthieu.fraiz@epitech.eu>
2023-03-25 10:40:59 +0100

    add: bouncing and shake

--- skis.py
+++ skis.py
@@ -1,4 +1,5 @@
 import pygame
+from random import uniform
 
 Surface = pygame.Surface
 Rect = pygame.Rect
@@ -22,17 +23,19 @@ def rotate(surface, angle, pivot, offset):
 class Ski:
 
     def __init__(self, img: Surface) -> None:
-        self.max_momentum = 150
-        self.momentum_base_factor = 180
+        self.max_momentum = 250
+        self.momentum_base_factor = 220
         self.momentum_reduction = 0.1
         self.angle = 90
         self.momentum = 0
+        self.bounce = False
+        self.center_offset = pygame.math.Vector2(0, img.get_height() // 4)
+
         self.img = img
-        self.center_offset = pygame.math.Vector2(0, 25)
 
     def set_angle(self, new_angle: float) -> float:
         if (new_angle > 180 or new_angle < 0):
-            self.momentum = 0
+            self.momentum = -self.momentum if self.bounce else 0
         self.angle = max(min(180, new_angle), 0)
         return self.angle
 
@@ -44,11 +47,12 @@ class Ski:
 
     def reduce_momentum(self, dt: float):
         self.momentum *= 1 - ((1 - self.momentum_reduction) * dt)
-        print(1 - ((1 - self.momentum_reduction) * dt))
-
 
     def add_momentum(self, factor: float) -> None:
         self.set_momentum(self.momentum + factor * self.momentum_base_factor)
+
+    def shake(self):
+        self.add_momentum(uniform(-self.max_momentum, self.max_momentum))
 
     def update(self, dt: float) -> tuple[Surface, Rect]:
         self.change_angle(self.momentum * dt)
@@ -80,8 +84,8 @@ class Skis:
         if keys[pygame.K_d]:
             self.left_ski.add_momentum(1 * dt)
 
-        y = screen.get_bounding_rect().height * self.y
-        width = screen.get_bounding_rect().width
+        y = screen.get_height() * self.y
+        width = screen.get_width()
         x_diff = width * self.x
         c = width / 2
 
@@ -93,10 +97,6 @@ class Skis:
         s, r = self.right_ski.get_rect((c + x_diff, y))
         screen.blit(s, r)
 
-
-
-
-
 s = Skis()
 pygame.init()
 screen = pygame.display.set_mode([500, 500])
@@ -104,10 +104,14 @@ running = True
 cl = pygame.time.Clock()
 
 while running:
-    dt = cl.tick(60) / 1000
+    dt = cl.tick(30) / 1000
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                s.left_ski.shake()
+
     screen.fill((255, 255, 255))
     s.update(screen, dt)
     pygame.display.update()
