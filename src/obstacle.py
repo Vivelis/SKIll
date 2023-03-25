486e057bc9361c204c14837ce15a5fc951d770c0
macgameur <maceo.jalbert@epitech.eu>
2023-03-25 11:57:25 +0100

    :fix: working obstacle spawner

--- src/obstacle.py
+++ src/obstacle.py
@@ -1,32 +1,39 @@
 import pygame
+import random
+from timeit import default_timer
 
 class obstacle :
-    def __init__(self, x, y, color) :
+    def __init__(self, x, y, w, h, color) :
         self.x = x
         self.y = y
+        self.w = w
+        self.h = h
         self.color = color
 
     def draw(self, screen) :
         pygame.draw.rect(screen, self.color, (self.x, self.y, self.w, self.h))
 
 class pomeranian(obstacle) :
-    def __init__(self, x, y, color) :
-        super().__init__(x, y, color)
+    def __init__(self, x, y, w, h, color) :
+        super().__init__(x, y, w, h, color)
         self.img = pygame.image.load("assets/pomeranian.png")
+        self.img = pygame.transform.scale(self.img, (w, h))
 
     def draw(self, screen):
-        super().draw(screen)
         screen.blit(self.img, (self.x, self.y))
 
 class obstacleSpawner :
     def __init__(self, screen) :
         self.screen = screen
         self.obstacles = []
-        self.clock = pygame.time.Clock()
-        self.spawn_rate = 1
+        self.last_spawn = default_timer()
+        self.spawn_time = 1
+        random.seed(10)
 
     def spawn(self) :
-        self.obstacles.append(pomeranian(0, 0, (255, 0, 0)))
+        x = random.randint(0, self.screen.get_width())
+        y = random.randint(0, self.screen.get_height())
+        self.obstacles.append(pomeranian(x, y, 100, 100, (255, 0, 0)))
 
     def draw(self) :
         for obstacle in self.obstacles :
@@ -34,7 +41,6 @@ class obstacleSpawner :
 
     def update(self) :
         self.draw()
-        if self.clock.get_time() > self.spawn_rate :
+        if default_timer() - self.last_spawn > self.spawn_time :
             self.spawn()
-            self.clock.tick()
-            print(self.clock.get_time())
+            self.last_spawn = default_timer()
